03add208cfc2ec1e0197f5468188461a57a9b232
navkant <navkant.tyagi9@gmail.com>
2025-11-22 14:23:00 +0530

    devTinder added few apis"

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -1,26 +1,54 @@
-const express = require("express");
+import express from "express";
+import connectDb from "./config/database.js";
+import User from "./models/user.js";
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
-app.use("/api", (req, res, next) => {
-  console.log("middleware called");
-  next();
+app.post("/signup", async (req, res) => {
+  const newUser = new User(req.body);
+  const newDbUser = await newUser.save();
+
+  res.send({
+    message: "success",
+    status: 0,
+    user: {
+      firstName: newDbUser.firstName,
+      lastName: newDbUser.lastName,
+      emailId: newDbUser.emailId,
+      password: newDbUser.password,
+      id: newDbUser._id,
+    },
+  });
 });
 
-app.get("/api/home", (req, res, next) => {
-  console.log("home controller");
-  throw Error("this is deprecated");
-  res.send("home controller");
+app.get("/users/:firstName", async (req, res) => {
+  const userDbList = await User.find({
+    firstName: { $regex: new RegExp(req.params.firstName, "i") },
+  });
+  const userList = userDbList.map((user, index) => {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailId: user.emailId,
+      password: user.password,
+    };
+  });
+  res.send({
+    message: "success",
+    status: 0,
+    userList: userList,
+  });
 });
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    console.log(err);
-    res.status(400).send("something went wrong");
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`express server listening at ${PORT}`);
-});
+connectDb()
+  .then(() => {
+    console.log("Connected to db server successfulyy");
+    app.listen(PORT, () => {
+      console.log(`express server listening at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Could not connect to db server ${err}`);
+  });
