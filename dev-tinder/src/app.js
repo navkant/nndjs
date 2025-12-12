2be7609fed945073888acd1a68c2f9eae7e77fb1
navkant <navkant.tyagi9@gmail.com>
2025-12-12 21:30:24 +0530

    added some validations

--- dev-tinder/src/app.js
+++ dev-tinder/src/app.js
@@ -8,108 +8,129 @@ app.use(express.json());
 const PORT = 3000;
 
 app.post("/signup", async (req, res) => {
-  const newUser = new User(req.body);
-  try {
-    const newDbUser = await newUser.save();
-    res.send({
-      message: "success",
-      status: 0,
-      user: {
-        firstName: newDbUser.firstName,
-        lastName: newDbUser.lastName,
-        emailId: newDbUser.emailId,
-        password: newDbUser.password,
-        id: newDbUser._id,
-      },
-    });
-  } catch (err) {
-    console.log(`err: ${err}`);
-    res.status(400).send(`could not create user: ${err}`);
-  }
+    const newUser = new User(req.body);
+    try {
+        const newDbUser = await newUser.save();
+        res.send({
+            message: "success",
+            status: 0,
+            user: {
+                firstName: newDbUser.firstName,
+                lastName: newDbUser.lastName,
+                emailId: newDbUser.emailId,
+                password: newDbUser.password,
+                id: newDbUser._id,
+            },
+        });
+    } catch (err) {
+        console.log(`err: ${err}`);
+        res.status(400).send(`could not create user: ${err}`);
+    }
 });
 
 app.get("/users", async (req, res) => {
-  const userDbList = await User.find({});
-  const userList = userDbList.map((user, index) => {
-    return {
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      emailId: user.emailId,
-      password: user.password,
-    };
-  });
-  res.send({
-    message: "success",
-    status: 0,
-    userList: userList,
-  });
+    const userDbList = await User.find({});
+    const userList = userDbList.map((user, index) => {
+        return {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            emailId: user.emailId,
+            password: user.password,
+        };
+    });
+    res.send({
+        message: "success",
+        status: 0,
+        userList: userList,
+    });
 });
 
 app.get("/users/:emailId", async (req, res) => {
-  const user = await User.findOne({
-    emailId: req.params.emailId,
-  });
-  res.send({
-    message: "success",
-    status: 0,
-    user: {
-      id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-    },
-  });
+    const user = await User.findOne({
+        emailId: req.params.emailId,
+    });
+    res.send({
+        message: "success",
+        status: 0,
+        user: {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+        },
+    });
 });
 
 app.delete("/users", async (req, res) => {
-  console.log("delete api called", req.query.userId);
-  const resp = await User.findByIdAndDelete(req.query.userId);
-  console.log(resp);
-  res.send({
-    message: "success",
-    status: 0,
-  });
+    console.log("delete api called", req.query.userId);
+    const resp = await User.findByIdAndDelete(req.query.userId);
+    console.log(resp);
+    res.send({
+        message: "success",
+        status: 0,
+    });
 });
 
 app.patch("/users", async (req, res) => {
-  console.log("patch api called");
-  console.log(`userId: ${req.query.userId} email: ${req.body.emailId}`);
-  const resp = await User.findByIdAndUpdate(req.query.userId, req.body, {
-    returnDocument: "after",
-  });
-  console.log(`object: ${resp}`);
-  res.send({
-    message: "success ",
-    status: "0",
-    user: {
-      firstName: resp.firstName,
-      lastName: resp.lastName,
-      emailId: resp.emailId,
-    },
-  });
+    console.log("patch api called");
+    const body = req.body;
+    const UPDATE_ALLOWED_FIELDS = ["lastName", "age", "gender"];
+    const isUpdateAllowed = Object.keys(body).every((key) =>
+        UPDATE_ALLOWED_FIELDS.includes(key)
+    );
+    if (!isUpdateAllowed) {
+        res.status(400).send({ message: "you cannot update this field" });
+        return;
+    }
+    try {
+        const resp = await User.findByIdAndUpdate(req.query.userId, body, {
+            returnDocument: "after",
+        });
+        if (!resp) {
+            res.status(400).send(`User doesnt exists: ${req.query.userId}`);
+            return;
+        }
+        res.send({
+            message: "success ",
+            status: "0",
+            user: {
+                firstName: resp.firstName,
+                lastName: resp.lastName,
+                emailId: resp.emailId,
+            },
+        });
+    } catch (err) {
+        console.log(`"error occured while updating user: ${err}`);
+        res.status(400).send(`Could not update user: ${err}`);
+    }
 });
 
 app.patch("/users/update-many", async (req, res) => {
-  console.log(
-    `update many called for ${req.query.emailId} ${JSON.stringify(req.body)}`
-  );
+    console.log(
+        `update many called for ${req.query.emailId} ${JSON.stringify(
+            req.body
+        )}`
+    );
 
-  const resp = await User.updateMany({ emailId: req.query.emailId }, req.body);
-  console.log(`update many response: ${resp}`);
+    const resp = await User.updateMany(
+        { emailId: req.query.emailId },
+        req.body
+    );
+    console.log(`update many response: ${resp}`);
 
-  res.send({
-    message: "updated many successfull",
-    status: 0,
-  });
+    res.send({
+        message: "updated many successfull",
+        status: 0,
+    });
 });
 
 connectDb()
-  .then(() => {
-    console.log("Connected to db server successfulyy");
-    app.listen(PORT, () => {
-      console.log(`express server listening at ${PORT}`);
+    .then(() => {
+        console.log("Connected to db server successfulyy");
+        app.listen(PORT, () => {
+            console.log(`express server listening at ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`Could not connect to db server ${err}`);
     });
-  })
-  .catch((err) => {
-    console.log(`Could not connect to db server ${err}`);
-  });
--- dev-tinder/src/models/user.js
+++ dev-tinder/src/models/user.js
@@ -1,30 +1,39 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 3,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    lastName: {
+        type: String,
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function (value) {
+                console.log(value);
+                if (!validator.isEmail(value)) {
+                    throw new Error()`Enter email id in correct format ${value}`;
+                }
+            },
+        },
+    },
+    password: {
+        type: String,
+    },
+    age: {
+        type: Number,
+    },
+    gender: {
+        type: String,
+    },
 });
 
 const User = mongoose.model("User", userSchema);
